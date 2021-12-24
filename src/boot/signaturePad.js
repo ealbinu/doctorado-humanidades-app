b7b975886547482728bf9e3c16b488f115ffe617
Albin Rdz <ealbinu@gmail.com>
2021-12-23 18:41:25 -0600

    Update & build

--- src/boot/signaturePad.js
+++
@@ -1,6 +0,0 @@
-import { boot } from 'quasar/wrappers'
-import VueSignaturePad from 'vue-signature-pad'
-
-export default boot(async ({app}) => {
-  app.use(VueSignaturePad)
-})
--- src/store/api/state.js
+++
@@ -1,8 +0,0 @@
-import {LocalStorage} from 'quasar'
-
-export default function () {
-  return {
-    user: JSON.parse(LocalStorage.getItem('iconosPL_user')) || null,
-    periodo: ''
-  }
-}
